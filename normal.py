32d3948805d81a3523e73dae8e3e310f3d7149bc
LordMethenor <lordmethenor@outlook.com>
2019-04-19 19:40:35 +0000

    working out bugs, merge, input errors

--- normal.py
+++ normal.py
@@ -3,6 +3,7 @@ import scipy
 from room import Room
 from inventory import Inv
 from inventory import Item
+
 class Normalize():
     # this is how random values are generated. They are generated on a normal distribution, with a mean as the 'base' spec and the standard deviation set to 15% of the mean by default but changeable. If an item with a certain rarity predetermined is generated, the returned result will specifically obey that. Values less than 0 are also eliminated.
     def __init__(self, mean, alt_deviation, rarity):
@@ -25,7 +26,7 @@ class Normalize():
             pass
     def draw_norm(self):
         draw_loop = True
-        while draw_loop = True:
+        while draw_loop == True:
             output_return = numpy.random.normal(loc=self.mean, scale=self.deviation, size=None)
             if output_return >= self.rarity:
                 draw_loop = False
@@ -33,10 +34,9 @@ class Normalize():
                 pass
         return output_return
 class Rarity_Check(Normalize):
-
-    def __init__(self, mean, alt_deviation, check_spec):
-        Normalize.__init__(self, mean, alt_deviation)
-        self.value = check_value
-    def check_rarity():
+    def __init__(self, mean, alt_deviation, check_spec, rarity):
+        Normalize.__init__(self, mean, alt_deviation, rarity)
+        self.value = check_spec
+    def check_rarity(self):
         output = scipy.stats.norm(self.mean, self.deviation).cdf(self.value) 
         return output
